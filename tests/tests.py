20500e30a6fd7c76a65d1b55bc12b5bc4a57cd07
Alexmorrison14 <alexmorrison141@gmail.com>
2022-07-21 13:48:24 +0100

    added a way to unit tests both sets of code

--- tests/tests.py
+++ tests/tests.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
 import string
-import better
+import better as test_code
 from hamcrest import assert_that, equal_to
 
 
 class TestAlphabetGenerator(TestCase):
     def test_all_letters_present(self):
-        output = better.generate_random_alphabet()
+        output = test_code.generate_random_alphabet()
         assert_that(set(output) >= set(string.ascii_lowercase))
 
     def test_only_26_letters_present(self):
-        output = better.generate_random_alphabet()
+        output = test_code.generate_random_alphabet()
         assert_that(len(output) == 26)
